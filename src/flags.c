ce36c0cfe38e11da7be69d9fe624b156193bac4b
andraspalasti <andraspalasti2@gmail.com>
2021-11-26 13:50:28 +0100

    created next arg function

--- src/flags.c
+++ src/flags.c
@@ -88,41 +88,57 @@ void print_flags_usage(FILE *out) {
     }
 }
 
+static char *next_arg(int *argc, char **argv[]) {
+    assert(*argc > 0);
+    char *result = **argv;
+    *argv += 1;
+    *argc -= 1;
+    return result;
+}
+
 void parse_flags(int argc, char *argv[]) {
-    for (int i = 1; i < argc; i++) {
+    // the first arg is always the execution so we can throw that away
+    next_arg(&argc, &argv);
+
+    while (argc > 0) {
         bool recognized = false;
+        char *arg = next_arg(&argc, &argv);
         for (int flag_idx = 0; flag_idx < flag_cnt; flag_idx++) {
 
             // parse: -flag_name
-            if (1 < strlen(argv[i]) && strcmp(argv[i] + 1, flags[flag_idx].name) == 0) {
+            if (1 < strlen(arg) && strcmp(arg + 1, flags[flag_idx].name) == 0) {
                 recognized = true;
                 Flag *flag = &(flags[flag_idx]);
 
                 // check if we are at the end of the flags
-                if (flag->type != FLAG_BOOL && i == argc - 1) {
-                    fprintf(stderr, "ERROR: You did not provide a flag value for: '%s'\n", argv[i]);
+                if (flag->type != FLAG_BOOL && argc == 0) {
+                    fprintf(stderr, "ERROR: You did not provide a flag value for: '%s'\n", arg);
                     print_flags_usage(stderr);
                     exit(EXIT_FAILURE);
                 }
 
-                // parse flags
+                // parse flag
                 switch (flag->type) {
-                    case FLAG_INT:
+                    case FLAG_INT: {
+                        char *val = next_arg(&argc, &argv);
                         // if sscanf did not successfully read in a value
-                        if (sscanf(argv[i + 1], "%d", &(flag->data.d)) != 1) {
-                            fprintf(stderr, "ERROR: Could not parse flag value: '%s', for flag '%s'\n", argv[i + 1], argv[i]);
+                        if (sscanf(val, "%d", &(flag->data.d)) != 1) {
+                            fprintf(stderr, "ERROR: Could not parse flag value '%s', for flag '%s'\n", val, arg);
                             print_flags_usage(stderr);
                             exit(EXIT_FAILURE);
                         }
                         break;
+                    }
 
                     case FLAG_BOOL:
                         flag->data.b = true;
                         break;
 
-                    case FLAG_STR:
-                        flag->data.str = argv[i + 1];
+                    case FLAG_STR: {
+                        char *val = next_arg(&argc, &argv);
+                        flag->data.str = val;
                         break;
+                    }
 
                     default:
                         assert(0 && "Unhandled flag type");
@@ -130,12 +146,12 @@ void parse_flags(int argc, char *argv[]) {
                 }
 
                 // we found the matching flag so break out from the loop
-                i++;
                 break;
             }
         }
+
         if (recognized == false) {
-            fprintf(stderr, "ERROR: Unrecognized flag: '%s'\n", argv[i]);
+            fprintf(stderr, "ERROR: Unrecognized flag: '%s'\n", arg);
             print_flags_usage(stderr);
             exit(EXIT_FAILURE);
         }
